471f691d542193430e6904083e4d6d8d91be5fe2
Mona Mohamed <30897431+Moon04@users.noreply.github.com>
2020-01-22 23:20:34 +0200

    Scripts

--- scripts/Hearts.js
+++ scripts/Hearts.js
@@ -4,7 +4,7 @@ heart.src = "../assets/heart.png";
 export function draw_heart(ctx, noOfHearts) {
     let heartX = 75;
     for (let i = 0; i < noOfHearts; i++) {
-        heartX += 40;
+        heartX += 35;
         ctx.drawImage(heart, heartX, 20, 25, 25);
     }
 }
--- scripts/ScoreBox.js
+++ scripts/ScoreBox.js
@@ -2,6 +2,6 @@ let score_box = new Image();
 score_box.src = "../assets/scoreBox.svg";
 
 export function draw_scoreBox(ctx) {
-    ctx.drawImage(score_box, 235, 10, 170, 47);
+    ctx.drawImage(score_box, 220, 10, 170, 47);
 
 }
